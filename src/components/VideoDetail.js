4af4b5da6471665cdc6d44e123464135afcd9c76
Borrison <borissoto@gmail.com>
2020-10-31 00:31:07 -0400

    showing video on iframe

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const VIdeoDetail = ({ video }) => {
-  return <div>{video.snippet.title}</div>;
+const VideoDetail = ({ video }) => {
+  if (!video) {
+    return <div>Loading...</div>;
+  }
+
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+  return (
+    <div>
+      <div className='ui embed'>
+        <iframe title='video player' src={videoSrc} />
+      </div>
+      <div className='ui segment'>
+        <h4 className='ui header'>{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
+    </div>
+  );
 };
 
-export default VIdeoDetail;
+export default VideoDetail;
